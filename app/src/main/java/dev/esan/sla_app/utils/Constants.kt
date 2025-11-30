b2d1e662dc151cf4e6a8865dd98a0924056e82fb
frayderMM <mezamorvelifrayder2005@gmail.com>
2025-11-30 14:49:51 -0500

    cambio de url

--- app/src/main/java/dev/esan/sla_app/utils/Constants.kt
+++ app/src/main/java/dev/esan/sla_app/utils/Constants.kt
@@ -2,5 +2,6 @@ package dev.esan.sla_app.utils
 
 object Constants {
     // La URL base solo debe apuntar al servidor. Las rutas completas se definen en las interfaces de la API.
-    const val BASE_URL = "http://10.0.2.2:5149/"
+    //const val BASE_URL = "http://10.0.2.2:5149/"
+    const val BASE_URL = "https://sla-api.onrender.com/"
 }
